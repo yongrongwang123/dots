6397c83b2403c0efef83c6e18c65356ecd07826e
yongrongwang123 <yongrongwang123@gmail.com>
2025-06-23 20:58:53 +0800

    add config of zsh, tmux, nvim, lazygit, alacritty, hyprland and waybar

---
+++ scripts/install-macos.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install coreutils gnu-tar findutils grep gnu-sed gawk wget node@22 tlrc
