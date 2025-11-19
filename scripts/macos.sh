416451b98e773832f128621f00685b3a15f263b7
yongrongwang123 <yongrongwang123@gmail.com>
2025-11-19 17:57:43 +0800

    add config of zsh

---
+++ scripts/macos.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install stow neovim tmux zsh-autosuggestions zsh-syntax-highlighting node ripgrep fd luarocks tree-sitter-cli lazygit diff-so-fancy
