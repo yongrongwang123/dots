c66f8ccf33ea52c4152fb71def08d28c138b7d6a
yongrongwang123 <yongrongwang123@gmail.com>
2025-11-19 17:56:34 +0800

    add config of hyprland and waybar

---
+++ scripts/hyprland.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo pacman -Syu --needed man-db man-pages zsh-autosuggestions zsh-syntax-highlighting stow neovim tmux fastfetch btop noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji ttf-sourcecodepro-nerd bluetui impala fwupd brightnessctl hyprpaper hyprlock hypridle polkit-gnome xdg-desktop-portal-gtk nemo rofi waybar ghostty zed chromium
+sudo pacman -Rsun nano uwsm htop wofi dunst kitty polkit-kde-agent dolphin
